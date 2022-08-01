92c0e5558cf73ef77a9746ae81a2b8c088fc9ca6
José Rodolpho <j.rodolpho29@hotmail.com>
2022-08-01 04:32:24 -0300

    feat: create create recommendation test

--- back-end/tests/recommendations.test.ts
+++ back-end/tests/recommendations.test.ts
@@ -5,8 +5,22 @@ import supertest from "supertest";
 describe("Create recommendantion", () => {
     it("Add a new recommendation and return status 201", async () => {
         const response = await supertest(app).post("/recommendations").send({
-            name: faker.name.findName() , youtubeLink: `www.youtube.com/${faker.random.alpha()}`
+            name: faker.music.songName() , youtubeLink: `www.youtube.com/${faker.random.alpha()}`
         })
         expect(response.status).toEqual(201)
+    })
+
+    it("Recommendation already exist and return status 409", async () => {
+        const response = await supertest(app).post("/recommendations").send({
+            name: "Herman Raynor" , youtubeLink: `www.youtube.com/s`
+        })
+        expect(response.status).toEqual(409)
+    })
+
+    it("Get recommendation wrong inputs and return status 422", async () => {
+        const response = await supertest(app).post("/recommendations").send({
+            name: 123 , youtubeLink: 321
+        })
+        expect(response.status).toEqual(422)
     })
 })
